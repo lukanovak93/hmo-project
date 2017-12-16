7fbef351f149ff6b8f96766aae3f0364a4e072ea
Domagoj Krivošić <domagoj_70@hotmail.com>
2017-12-16 22:33:07 +0100

    Sort population before cloning

--- CloneOperator.cpp
+++ CloneOperator.cpp
@@ -8,6 +8,7 @@ CloneOperator::CloneOperator(float beta) : _beta(beta)
 
 std::vector<Antibody> CloneOperator::clone(std::vector<Antibody> population)
 {
+    std::sort(population.begin(), population.end());
     int populationSize = population.size();
     std::vector<Antibody> clones;
 
